c288576afbe93258270c0e77297af14c5c5d7fcf
Edward Ho <pythoned77@gmail.com>
2022-08-27 09:54:24 +0800

    added get and post for CRUD, added middleware

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 
@@ -9,11 +11,25 @@ const db = mysql.createPool({
   database: "CRUDDataBase",
 });
 
-app.get("/", (req, res) => {
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/api/get", (req, res) => {
+  const sqlSelect = "SELECT * FROM movie_reviews";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
+});
+
+app.post("/api/insert", (req, res) => {
+  const movieName = req.body.movieName;
+  const movieReview = req.body.movieReview;
+
   const sqlInsert =
-    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('inception', 'good movie');";
-  db.query(sqlInsert, (err, result) => {
-    res.send("hello World");
+    "INSERT INTO movie_reviews(movieName, movieReview) VALUES (?,?)";
+  db.query(sqlInsert, [movieName, movieReview], (err, result) => {
+    console.log(result);
   });
 });
 
